56374d1a3c37d352aa8f0e0b9ecff490add9f26e
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-05-02 07:27:41 +0200

    feat(vm) add of the help message for the corewar

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -5,7 +5,16 @@
 ** main.c
 */
 
-int main(void)
+#include <stdio.h>
+#include "ice/string.h"
+#include "ice/printf.h"
+#include "corewar/corewar.h"
+
+int main(int ac, char **av)
 {
+    FILE *file;
+
+    if (ac == 2 && (!ice_strcmp(av[1], "-h") || !ice_strcmp(av[1], "--help")))
+        ice_printf(HELP_MESSAGE);
     return 0;
 }
