e0db9ab90d10b8420580b744de6f56c0787f7510
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-05-12 09:36:38 +0200

    feat(vm) add of the linked list for champion on the .h

--- include/corewar/corewar.h
+++ include/corewar/corewar.h
@@ -8,8 +8,10 @@
 #ifndef COREWAR_COREWAR_H
     #define COREWAR_COREWAR_H
 
+    #include "op.h"
+    #include <sys/queue.h>
+
     #define IS_NUM(c) (c >= '0' && c <= '9')
-    #define IS_STR_NUM(str) (str[0] == '-' ? IS_NUM(str[1]) : IS_NUM(str[0]))
 
 typedef struct arg_parse_s {
     int dump;
@@ -17,8 +19,15 @@ typedef struct arg_parse_s {
     int load_address;
 } arg_parse_t;
 
+typedef struct champion_s {
+    header_t header;
+    char *prog;
+    TAILQ_ENTRY(champion_s) next;
+} champion_t;
+
 typedef struct corewar_s {
     arg_parse_t arg_parse;
+    TAILQ_HEAD(champion_head_s, champion_s) champions;
 } corewar_t;
 
 int parse_arg(corewar_t *corewar, int ac, char **av);
