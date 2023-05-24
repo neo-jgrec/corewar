32f9148537b02b89d425e0cfca944fa903b00b3b
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2023-05-24 19:24:36 +0200

    fix(vm) fix usage printing

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -17,35 +17,36 @@ void parse_args(int ac, char **av, vm_t *vm);
 void vm_init(vm_t *vm);
 void pre_vm(vm_t *vm);
 
-static const char *help_message =
-"USAGE\n"
-"./corewar"
-"[-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n"
-"DESCRIPTION\n"
-"-dump nbr_cycle dumps the memory after the nbr_cycle execution (if the round"
-"isn't already over) with the following format: 32 bytes/line in hexadecimal"
-"(A0BCDEFE1DD3...)\n"
-"-n prog_number sets the next program's number. By default, the first free"
-"number in the parameter order\n"
-"-a load_address sets the next program's loading address. When no address is"
-"specified, optimize the addresses so that the processes are as far"
-"away from each other as possible. The addresses are MEM_SIZE modulo.\n";
+static const char help_message[] = "USAGE\n"
+    "./corewar "
+    "[-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n"
+    "DESCRIPTION\n"
+    "-dump nbr_cycle dumps the memory after the nbr_cycle execution "
+    "(if the round isn't already over) with the following format: "
+    "32 bytes/line\n"
+    "in hexadecimal (A0BCDEFE1DD3...)\n"
+    "-n prog_number sets the next program's number. By default, the first free"
+    " number in the parameter order\n"
+    "-a load_address sets the next program's loading address. When no address "
+    "is specified, optimize the addresses so that the processes are as far "
+    "away from each other as possible. The addresses are MEM_SIZE modulo.\n";
 
 int main(int ac, char **av)
 {
     vm_t vm = {0};
 
     if (ac < 2 || (!ice_strcmp(av[1], "-h") || !ice_strcmp(av[1], "--help"))) {
-        fwrite(help_message, 1, ice_strlen(help_message), stderr);
-    } else {
-        vm_init(&vm);
-        TAILQ_INIT(&vm.champ_list);
-        parse_args(ac, av, &vm);
-        vm.cycle_to_die = (vm.dump_cycle == 0) ? CYCLE_TO_DIE : vm.dump_cycle;
-        pre_vm(&vm);
-        #if defined(DEBUG)
-            print_debug(&vm);
-        #endif
+        fwrite(help_message, sizeof(help_message), 1,
+            ((ac < 2) ? stderr : stdout));
+        return ((ac < 2) ? 84 : 0);
     }
+    vm_init(&vm);
+    TAILQ_INIT(&vm.champ_list);
+    parse_args(ac, av, &vm);
+    vm.cycle_to_die = (vm.dump_cycle == 0) ? CYCLE_TO_DIE : vm.dump_cycle;
+    pre_vm(&vm);
+    #if defined(DEBUG)
+        print_debug(&vm);
+    #endif
     return (0);
 }
