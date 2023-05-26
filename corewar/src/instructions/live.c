e4f82fe33fd58055e7d67acec9a51f81fd960c44
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2023-05-26 14:09:12 +0200

    fix(vm) removed parameter coding bytecheck from live

--- corewar/src/instructions/live.c
+++ corewar/src/instructions/live.c
@@ -13,8 +13,6 @@ void live(vm_t *vm, champion_t *champ, process_t *process)
 {
     uint32_t number = 0;
 
-    if (*(process->pc++) != 0b10000000u)
-        kill_process(vm, champ, process);
     for (uint8_t i = 0; i < 4; i++)
         number |= *(process->pc++) << (i * 8);
     TAILQ_FOREACH(champ, &vm->champ_list, entries)
