cade9e7ac66ded4a6a556dff1634bb3f15dda754
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-05-28 11:59:31 +0200

    fix(vm_v3) alive instruction

--- corewar/src/instructions/live.c
+++ corewar/src/instructions/live.c
@@ -6,9 +6,11 @@
 */
 
 #include "corewar/corewar.h"
+#include "ice/printf.h"
 
 void live(vm_t *vm, champion_t *champ, process_t *process)
 {
+    static const char format[] = "The player %d(%s)is alive.\n";
     uint32_t number = 0;
 
     for (uint8_t i = 0; i < 4; i++)
@@ -17,6 +19,7 @@ void live(vm_t *vm, champion_t *champ, process_t *process)
         if (champ->number == number) {
             champ->alive = true;
             vm->live_call_count++;
+            ice_printf(format, champ->number, champ->name);
             break;
         }
 }
